9edf7c86aeda1362559d2746fd633490450d479a
Vladlen Fedosov <vlad.fedosov@gmail.com>
2013-05-27 00:38:59 +0300

    First commit

---
+++ C_Sharp_Kursovoi_3_kurs/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace C_Sharp_Kursovoi_3_kurs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.UrlInput = new System.Windows.Forms.TextBox();
+			this.StartButton = new System.Windows.Forms.Button();
+			this.LogInput = new System.Windows.Forms.TextBox();
+			this.loadProgress = new System.Windows.Forms.ProgressBar();
+			this.SuspendLayout();
+			// 
+			// UrlInput
+			// 
+			this.UrlInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.UrlInput.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.UrlInput.Location = new System.Drawing.Point(12, 12);
+			this.UrlInput.Name = "UrlInput";
+			this.UrlInput.Size = new System.Drawing.Size(617, 20);
+			this.UrlInput.TabIndex = 0;
+			this.UrlInput.Text = "http://chevrolet-aveo.ru/showthread.php?t=%(861-863)%#%(01/06/2013-05/06/2013)%";
+			// 
+			// StartButton
+			// 
+			this.StartButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.StartButton.Location = new System.Drawing.Point(635, 10);
+			this.StartButton.Name = "StartButton";
+			this.StartButton.Size = new System.Drawing.Size(75, 23);
+			this.StartButton.TabIndex = 1;
+			this.StartButton.Text = "Start";
+			this.StartButton.UseVisualStyleBackColor = true;
+			this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+			// 
+			// LogInput
+			// 
+			this.LogInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.LogInput.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.LogInput.Location = new System.Drawing.Point(12, 57);
+			this.LogInput.Multiline = true;
+			this.LogInput.Name = "LogInput";
+			this.LogInput.ReadOnly = true;
+			this.LogInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.LogInput.Size = new System.Drawing.Size(698, 262);
+			this.LogInput.TabIndex = 2;
+			this.LogInput.WordWrap = false;
+			// 
+			// loadProgress
+			// 
+			this.loadProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.loadProgress.Location = new System.Drawing.Point(12, 38);
+			this.loadProgress.Name = "loadProgress";
+			this.loadProgress.Size = new System.Drawing.Size(698, 13);
+			this.loadProgress.Step = 1;
+			this.loadProgress.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+			this.loadProgress.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(722, 331);
+			this.Controls.Add(this.loadProgress);
+			this.Controls.Add(this.LogInput);
+			this.Controls.Add(this.StartButton);
+			this.Controls.Add(this.UrlInput);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "URL Downloader";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UrlInput;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.TextBox LogInput;
+		private System.Windows.Forms.ProgressBar loadProgress;
+    }
+}
+
---
+++ C_Sharp_Kursovoi_3_kurs/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Kursovoi_3_kurs
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private async void StartButton_Click(object sender, EventArgs e)
+		{
+			string save_folder="";
+			string url_mask= UrlInput.Text;
+			LogInput.Text="";
+			///
+			//Проверка данных
+			if(url_mask=="") {
+				MessageBox.Show("Enter the mask URL of the resource");
+				return;
+			}
+
+			//Получаем папку для сохранения файлов
+			using (FolderBrowserDialog dialog = new FolderBrowserDialog()){
+				dialog.Description = "Folder to save the files";
+				dialog.RootFolder = Environment.SpecialFolder.MyDocuments;
+				if (dialog.ShowDialog() == DialogResult.OK){
+					save_folder = dialog.SelectedPath;
+				}else{
+					return;
+				}
+			}
+			Log("Save folder: "+save_folder);
+
+			//Блокируем кнопку старта
+			StartButton.Enabled= false;
+
+			//Получаем список ссылок из сформированного шаблона
+			List<string> parsed_urls= ParseUrl(url_mask);
+
+			//Задаем положение прогрессбара
+			loadProgress.Maximum= parsed_urls.Count;
+			loadProgress.Value=0;
+
+			//Загружаем ссылки
+			foreach(string url in parsed_urls){
+				Stream resp= null;
+				int request_code = await Task.Run(() => MakeRequest(url, ref resp));
+				if(request_code!=200) {
+					Log("Request URI: "+url+" [FAILED] Error code: "+request_code+".");
+				} else {
+					Log("Request URI: "+url+" [OK]");
+				}
+				string filepath= save_folder+"\\"+MakeName(url)+".html";
+				using(Stream file = File.OpenWrite(filepath)) {
+					CopyStream(resp, file);
+					Log("Saved to: "+filepath);
+				}
+				loadProgress.Value+=1;
+			}
+
+			//Разблокирываем кнопку старта
+			StartButton.Enabled= true;
+		}
+
+		private List<string> ParseUrl(string input) 
+		{
+			string pattern = @"%\(.*?\)%";
+			List<string> urls= new List<string> { input };
+			///
+			for(var ij = 0; ij < urls.Count; ij++) {
+				foreach(Match match in Regex.Matches(urls[ij], pattern)) {
+					List<string> new_urls= new List<string>();
+					///
+					string input_1= urls[ij].Substring(0, match.Index);
+					string input_2= urls[ij].Substring(match.Index+match.Value.Length);
+
+					//Выделяем начальное значение, и конечное
+					string match_inner = match.Value.Replace("%(", "").Replace(")%", "");
+					string[] inner_arr = match_inner.Split(new Char[]{'-'});
+					string start=inner_arr[0], finish= inner_arr[1];
+
+					//Обрабатываем маску согласно её типу
+					if(Regex.IsMatch(match_inner, @"^\d+\-\d+$")) {
+						int start_int=Convert.ToInt32(start), finish_int= Convert.ToInt32(finish);
+						///
+						for(var ii = start_int; ii <= finish_int; ii++) {
+							new_urls.Add(input_1+ii+input_2);
+						}
+					} else if(Regex.IsMatch(match_inner, @"^\d\d/\d\d/\d\d\d\d\-\d\d/\d\d/\d\d\d\d$")) {
+						DateTime startDate = DateTime.Parse(start);
+						DateTime endDate = DateTime.Parse(finish);
+						///
+						while(DateTime.Compare(startDate, endDate)<=0) {
+							new_urls.Add(input_1+startDate.ToString(@"dd\/MM\/yyyy")+input_2);
+							startDate= startDate.AddDays(1);
+						}
+					}
+
+					urls.RemoveAt(ij);
+					urls.AddRange(new_urls);
+					ij=-1;
+					break;
+				}
+			}
+			return urls;
+		}
+
+		private int MakeRequest(string url, ref Stream resp) 
+		{
+			HttpWebRequest HttpWReq = (HttpWebRequest)WebRequest.Create(url);
+			HttpWReq.Method = "GET";
+			HttpWReq.MaximumAutomaticRedirections = 4;
+			HttpWebResponse response = (HttpWebResponse)HttpWReq.GetResponse();
+			if(response.StatusCode != HttpStatusCode.OK) {
+				return (int)response.StatusCode;
+			}
+			resp= response.GetResponseStream();
+			return (int)response.StatusCode;
+		}
+
+		private string MakeName(string name)
+		{
+			foreach(char c in System.IO.Path.GetInvalidFileNameChars()) {
+				name = name.Replace(c, '_');
+			}
+			return name;
+		}
+
+		private void CopyStream(Stream input, Stream output)
+		{
+			byte[] buffer = new byte[8 * 1024];
+			int len;
+			while ((len = input.Read(buffer, 0, buffer.Length)) > 0)
+			{
+				output.Write(buffer, 0, len);
+			}
+		}
+
+		private void Log(string str) 
+		{
+			LogInput.Text += str+"\r\n";
+		}
+	}
+}
